84b34217c8791b8b20275294db58aed1da54941d
Christopher Gu <christopher.gu@gmail.com>
2014-05-13 23:15:03 -0500

    Initial Commit

---
+++ LogInTutorial/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LogInTutorial
+//
+//  Created by Christopher Gu on 5/13/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
