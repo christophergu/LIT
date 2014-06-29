e37926b7b0b4c8860c68fed4f11a7d0e7269bb00
Christopher Gu <christopher.gu@gmail.com>
2014-06-29 12:41:43 -0500

    searching leaders works, next segue to search leaders profile

---
+++ LogInTutorial/SearchResultsTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultsTableViewCell.h
+//  LIT
+//
+//  Created by Christopher Gu on 6/29/14.
+//  Copyright (c) 2014 Christopher Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultsTableViewCell : UITableViewCell
+
+@end
--- LogInTutorial/SearchResultsViewController.m
+++ LogInTutorial/SearchResultsViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "SearchResultsViewController.h"
+#import "SearchResultsTableViewCell.h"
+#import <Parse/Parse.h>
 
 @interface SearchResultsViewController ()<UITableViewDataSource, UITableViewDelegate>
+@property (copy, nonatomic) NSArray *searchResultsArray;
+@property (weak, nonatomic) IBOutlet UITableView *myTableView;
 
 @end
 
@@ -18,17 +22,36 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    PFQuery *usersWithMatchingTagsQuery = [PFUser query];
+    [usersWithMatchingTagsQuery whereKey:@"tags" containsAllObjectsInArray:self.selectedTagsArray];
+    [usersWithMatchingTagsQuery findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        self.searchResultsArray = objects;
+        [self.myTableView reloadData];
+    }];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.selectedTagsArray.count;
+    return self.searchResultsArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ResultsCellReuseID"];
-    cell.textLabel.text = self.selectedTagsArray[indexPath.row];
+    SearchResultsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ResultsCellReuseID"];
+    
+    if (self.searchResultsArray[indexPath.row][@"avatar"])
+    {
+        [self.searchResultsArray[indexPath.row][@"avatar"] getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+            if (!error) {
+                UIImage *photo = [UIImage imageWithData:data];
+                cell.myImageView.image = photo;
+            }
+        }];
+    }
+    
+    cell.myExpertiseLabel.text = self.searchResultsArray[indexPath.row][@"expertise"];
+    cell.myUsernameLabel.text = self.searchResultsArray[indexPath.row][@"username"];
     return cell;
 }
 
